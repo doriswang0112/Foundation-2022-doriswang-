01814a6851e0804c0e97c1d7327d2b212d29a1e8
doriswang0112 <doriswang@cca.edu>
2022-03-17 17:31:40 -0700

    for project 2

---
+++ Projects/Project-02/app.js
@@ -0,0 +1,8 @@
+const logo =document.querySelectorAll(selectors:'#logo path');
+
+for (let i = 0; i < logo.length; i++) {
+    console.log('Letter ${i} is ${logo[i].getTotalLength()}');
+    
+    
+    
+}
