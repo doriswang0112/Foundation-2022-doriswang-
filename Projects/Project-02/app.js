467f69aca2019a593a47e7384773b772a83f2ffb
Doris Wang <98133609+doriswang0112@users.noreply.github.com>
2022-04-18 21:56:25 -0700

    Delete Projects/Project-02 directory

--- Projects/Project-02/app.js
+++
@@ -1,8 +0,0 @@
-const logo =document.querySelectorAll(selectors:'#logo path');
-
-for (let i = 0; i < logo.length; i++) {
-    console.log('Letter ${i} is ${logo[i].getTotalLength()}');
-    
-    
-    
-}
